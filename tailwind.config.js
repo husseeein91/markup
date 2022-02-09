c142c6913646d0513c0f98878f40489b92a32da4
Mohamed Hussein <husseeein91@gmail.com>
2022-02-09 16:25:01 +0200

    Finalizing Markup

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,14 +10,42 @@ module.exports = {
       grey: "#464346",
       white: "#ffffff",
       lightGrey: "#5D5D5D",
+      mc: "#3F3D56",
+      nc: "#E5E5E5",
+      nb: "#5D5D5D",
+      bb: "#2F2E41",
+      mcm: "#464346",
+      ce: "#EEEEEE",
+      fb: "url('assets/bg.png')",
     },
     backgroundImage: {
       "hero-pattern": "url('../src/images/bg.png')",
     },
     fontFamily: {
+      Roboto: ["Roboto"],
       sans: ["Poppins", "sans-serif"],
     },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        ".container": {
+          maxWidth: "100%",
+          "@screen sm": {
+            maxWidth: "640px",
+          },
+          "@screen md": {
+            maxWidth: "768px",
+          },
+          "@screen lg": {
+            maxWidth: "1280px",
+          },
+          "@screen xl": {
+            maxWidth: "1280px",
+          },
+        },
+      });
+    },
+  ],
 };
